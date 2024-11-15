facb3bd2b57baf9dc47f2fa767106e94d45618b4
danielnales <dpnales@gmail.com>
2024-11-15 16:18:47 +0100

    Added Client and Contact model

---
+++ Backend/Features/Clients/Client.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Features.Clients
+{
+    [Table("Clients")]
+    public class Client
+    {
+        [Key]
+        public required int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required string Address { get; set; }
+
+        [Required]
+        public required string City { get; set; }
+
+        [Required]
+        public required string ZipCode { get; set; }
+
+        [Required]
+        public required string Country { get; set; }
+
+        [Required]
+        public required Contact Contact { get; set; }
+    }
+}
---
+++ Backend/Features/Clients/Contact.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Features.Clients
+{
+    public class Contact
+    {
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required]
+        [Phone]
+        public required string Phone { get; set; }
+    }
+}
