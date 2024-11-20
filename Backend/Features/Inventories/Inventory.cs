84a7163235f3dafecc6ef086c51084407f7b5ddb
ItzHarold <haroldponte2003@gmail.com>
2024-11-20 10:54:34 +0100

    Fixed format for runner to succeed
    It kept complaining about needing a whitespace

--- Backend/Features/Inventories/Inventory.cs
+++ Backend/Features/Inventories/Inventory.cs
@@ -10,17 +10,23 @@ namespace Backend.Features.Inventories
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public required int Id { get; set; }
 
-        [Required] [ForeignKey("Items")] public required string ItemId { get; set; }
+        [Required]
+        [ForeignKey("Items")] public required string ItemId { get; set; }
 
-        [Required] public required int TotalOnHand { get; set; }
+        [Required]
+        public required int TotalOnHand { get; set; }
 
-        [Required] public required int TotalExpected { get; set; }
+        [Required]
+        public required int TotalExpected { get; set; }
 
-        [Required] public required int TotalOrdered { get; set; }
+        [Required]
+        public required int TotalOrdered { get; set; }
 
-        [Required] public required int TotalAllocated { get; set; }
+        [Required]
+        public required int TotalAllocated { get; set; }
 
-        [Required] public required int TotalAvailable { get; set; }
+        [Required]
+        public required int TotalAvailable { get; set; }
 
         public string? Description { get; set; }
     }
