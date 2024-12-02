7e13d88955648d81997a05c4e4d9b7ee6252de1d
ItzHarold <haroldponte2003@gmail.com>
2024-12-02 22:29:30 +0100

    Fixed the formatting according to ubuntu

--- Backend.IntegrationTests/Controllers/HealthControllerTest.cs
+++ Backend.IntegrationTests/Controllers/HealthControllerTest.cs
@@ -1,22 +1,24 @@
-using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using Backend.Controllers;
+using Microsoft.AspNetCore.Mvc;
 using Xunit;
 
-namespace Backend.IntegrationTests.Controllers;
-
-public class HealthControllerTest
+namespace Backend.IntegrationTests.Controllers
 {
-    [Fact]
-    public async Task HealthControllerOnSuccessReturns200()
+    public class HealthControllerTest
     {
-        // Arrange
-        var controller = new HealthController();
+        [Fact]
+        public async Task HealthControllerOnSuccessReturns200()
+        {
+            // Arrange
+            var controller = new HealthController();
 
-        // Act
-        var result = await Task.FromResult(controller.HealthCheck()) as OkResult;
+            // Act
+            var result = await Task.FromResult(controller.HealthCheck()) as OkResult;
 
-        // Assert
-        Assert.NotNull(result);
-        Assert.Equal(200, result.StatusCode);
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+        }
     }
 }
