fcb9d1ec8482e8c5811e76d6eab5badb330f4538
ItzHarold <haroldponte2003@gmail.com>
2024-12-03 15:52:21 +0100

    Implemented the GetbyID Unittest

--- Backend.IntegrationTests/Controllers/WarehousesControllerTest.cs
+++ Backend.IntegrationTests/Controllers/WarehousesControllerTest.cs
@@ -20,5 +20,20 @@ namespace Backend.IntegrationTests.Controllers
             // Assert
             result.StatusCode.Should().Be(HttpStatusCode.OK);
         }
+
+        [Fact]
+        public async Task GetWarehouseByIdOnSuccessReturns204()
+        {
+            // Arrange
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("X-API-KEY", "3f5e8b9c-2d4a-4b6a-8f3e-1a2b3c4d5e6f");
+            // Check out options TODO
+
+            // Act
+            var result = await client.GetAsync("http://localhost:5031/api/warehouses/1");
+
+            // Assert
+            result.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        }
     }
 }
