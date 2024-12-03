0025571125c44affe0cae439f0f713c7a68bf0eb
ItzHarold <haroldponte2003@gmail.com>
2024-12-03 15:33:47 +0100

    Implemented the necessary tests
    Since i implemented the Controller and services in another branch, i only need to make tests for warehouses

--- Backend.IntegrationTests/Controllers/WarehousesControllerTest.cs
+++ Backend.IntegrationTests/Controllers/WarehousesControllerTest.cs
@@ -1,25 +1,24 @@
-// using System.Threading.Tasks;
-// using Backend.Controllers.Warehouses;
-// using Backend.Features.Warehouses;
-// using Microsoft.AspNetCore.Mvc;
-// using Xunit;
-//
-// namespace Backend.IntegrationTests.Controllers
-// {
-//     public class WarehousesControllerTest
-//     {
-//         [Fact]
-//         public async Task GetAllWarehousesOnSuccessReturns200()
-//         {
-//             // Arrange
-//             var controller = new WarehousesController();
-//
-//             // Act
-//             var result = await Task.FromResult(controller.GetAllWarehouses()) as OkObjectResult;
-//
-//             // Assert
-//             Assert.NotNull(result);
-//             Assert.Equal(200, result.StatusCode);
-//         }
-//     }
-// }
+using System.Net;
+using FluentAssertions;
+using Xunit;
+
+namespace Backend.IntegrationTests.Controllers
+{
+    public class WarehousesControllerTest
+    {
+        [Fact]
+        public async Task GetAllWarehousesOnSuccessReturns200()
+        {
+            // Arrange
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("X-API-KEY", "3f5e8b9c-2d4a-4b6a-8f3e-1a2b3c4d5e6f");
+            // Check out options TODO
+
+            // Act
+            var result = await client.GetAsync("http://localhost:5031/api/warehouses");
+
+            // Assert
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
