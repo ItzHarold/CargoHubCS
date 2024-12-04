44a97a2d9fb4cc3819dbd2b59fc8a7f8375796f9
Daniël <124064665+danielnales@users.noreply.github.com>
2024-12-04 15:00:02 +0100

    Added GetAllItems and AddItem functionality

--- Backend/Features/Items/ItemService.cs
+++ Backend/Features/Items/ItemService.cs
@@ -1,20 +1,34 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
+using Backend.Infrastructure.Database;
 
 namespace Backend.Features.Items
 {
     public interface IItemService
     {
-        // IEnumerable<Item> GetAllItems();
+        IEnumerable<Item> GetAllItems();
         // Item? GetItemById(int id);
-        // void AddItem(Item item);
+        void AddItem(Item item);
         // void UpdateItem(Item item);
         // void DeleteItem(int id);
     }
 
     public class ItemService : IItemService
     {
+        private readonly CargoHubDbContext _context;
 
+        public ItemService(CargoHubDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Item> GetAllItems() => _context.Items.ToList();
+
+        public void AddItem(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+        }
     }
 }
