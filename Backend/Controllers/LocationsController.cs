636fa551e09d8f798fc58d25ca34984fc7c9c431
JurDev <jurwinevertsz@live.nl>
2024-12-04 14:27:10 +0100

    Location Get

--- Backend/Controllers/LocationsController.cs
+++ Backend/Controllers/LocationsController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Backend.Features.Locations;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers.Locations
@@ -7,6 +8,18 @@ namespace Backend.Controllers.Locations
     [Route("api/[controller]")]
     public class LocationsController : ControllerBase
     {
+        private readonly ILocationService _locationService;
 
+        public LocationsController(ILocationService locationService)
+        {
+            _locationService = locationService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllClients()
+        {
+            var locations = _locationService.GetAllLocations();
+            return Ok(locations);
+        }
     }
 }
