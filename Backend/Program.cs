dc4e71aa4ee4e3d210d90a7007e50eb808a7aee7
Charriexx <charlotte.e.kwok@gmail.com>
2024-12-04 15:21:17 +0100

    Get all item groups

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -60,7 +60,7 @@ public static class Program
         services.AddSingleton<ILocationService, LocationService>();
         services.AddSingleton<IItemService, ItemService>();
         services.AddSingleton<IInventoryService, InventoryService>();
-        //services.AddSingleton<IItemGroupService,ItemGroupService>();
+        services.AddSingleton<IItemGroupService,ItemGroupService>();
         // services.AddSingleton<IItemLineService,ItemLineService>();
         // services.AddSingleton<IItemTypeService,ItemTypeService>();
         // services.AddSingleton<IOrderService,OrderService>();
