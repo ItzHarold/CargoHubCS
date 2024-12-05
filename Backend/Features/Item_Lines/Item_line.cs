c50085445ae8d16367ad51f2b5142e2e26a699a6
Daniël <124064665+danielnales@users.noreply.github.com>
2024-12-05 21:36:04 +0100

    Added item lines post functionality

--- Backend/Features/Item_Lines/Item_line.cs
+++ Backend/Features/Item_Lines/Item_line.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace Backend.Features.ItemLines
 {
@@ -8,11 +9,14 @@ namespace Backend.Features.ItemLines
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public required int Id { get; set; }
+        [JsonPropertyName("uid")]
+        public required string uid { get; set; }
 
         [Required]
+        [JsonPropertyName("name")]
         public required string Name { get; set; }
 
+        [JsonPropertyName("description")]
         public string? Description { get; set; }
     }
 }
