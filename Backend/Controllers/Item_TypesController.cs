6fea5d5c253fdc330b49d9b7ff4a5563b6c07387
Daniël <124064665+danielnales@users.noreply.github.com>
2024-12-07 03:19:55 +0100

    Added getall functionality for item types

--- Backend/Controllers/Item_TypesController.cs
+++ Backend/Controllers/Item_TypesController.cs
@@ -1,12 +1,24 @@
 using System.Collections.Generic;
+using Backend.Features.ItemTypes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers.ItemTypes
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/item_types")]
     public class ItemTypesController : ControllerBase
     {
+        private IItemTypeService _itemTypeService;
 
+        public ItemTypesController(IItemTypeService itemTypeService)
+        {
+            _itemTypeService = itemTypeService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllItemTypes()
+        {
+            return Ok(_itemTypeService.GetAllItemTypes());
+        }
     }
 }
